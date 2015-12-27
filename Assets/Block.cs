3293c15f948dccc8c83c522fd6dcd9d231856d86
Gyusun Yeom <omniavinco@gmail.com>
2015-12-27 15:48:29 +0900

    Add block, stage - default data

---
+++ Assets/Block.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+	public Color color { get; set; }
+
+	public Block() {
+		color = Color.black;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
---
+++ Assets/Stage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage : MonoBehaviour {
+	public int Size { get; set; }
+	private Block[] World { get; set; }
+
+	public Stage(int size) {
+		Size = size;
+		World = new Block[size * size];
+	}
+
+	public Block block(int x, int y) {
+		return World[y * Size + x];
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
