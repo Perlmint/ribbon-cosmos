66f3506ff6a061f091e2951f592ff5291313c3d8
Gyusun Yeom <omniavinco@gmail.com>
2016-02-07 10:11:51 +0900

    Prevent error for non-editor build
    Editor extensions are only available on editor build

---
+++ Assets/Editor/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Block))]
+public class BlockEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		Block block = (Block)target;
+
+		block.color = EditorGUILayout.ColorField("Color", block.color);
+	}
+}
---
+++ Assets/Editor/Ribbon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEditor;
+
+[CustomEditor(typeof(Ribbon))]
+public class RibbonEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		Ribbon ribbon = (Ribbon)target;
+
+		ribbon.color = EditorGUILayout.ColorField("Color", ribbon.color);
+		ribbon.width = EditorGUILayout.IntField("Width", ribbon.width);
+	}
+}
